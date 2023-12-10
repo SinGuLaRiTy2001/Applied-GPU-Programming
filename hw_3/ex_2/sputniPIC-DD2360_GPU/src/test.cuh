1175bf695e923a5c1d5b8e29d433675cf0a06bbd
KH200012 <117527862+KH200012@users.noreply.github.com>
2023-12-10 15:27:40 +0100

    Add files via upload

---
+++ hw_3/ex_2/sputniPIC-DD2360_GPU/src/test.cuh
@@ -0,0 +1,14 @@
+//
+// Created by jerry on 2023/12/8.
+//
+
+#ifndef SPUTNIPIC_DD2360_MASTER_TEST_CUH
+#define SPUTNIPIC_DD2360_MASTER_TEST_CUH
+
+
+class test {
+
+};
+
+
+#endif //SPUTNIPIC_DD2360_MASTER_TEST_CUH
